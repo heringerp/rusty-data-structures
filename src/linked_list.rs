ecdab81c2e4c6d8ca49e6ed1d1349adb2bbca27b
Peter Heringer <heringer@protonmail.com>
2022-11-23 22:29:05 +0100

    Add different directions + index get for linked list

--- src/linked_list.rs
+++ src/linked_list.rs
@@ -1,4 +1,4 @@
-enum Node<T> {
+pub enum Node<T> {
     None,
     Tail { value: T },
     Link { value: T, next: Box<Self> }, 
@@ -10,6 +10,34 @@ impl<T> Node<T> where T: Copy {
             Self::None => { self.to_tail(value) },
             Self::Tail { value:_ } => { self.to_link(value) },
             Self::Link { value:_, next } => { next.push(value) },
+        }
+    }
+
+    pub fn lpush(&mut self, value: T) {
+        match self {
+            Self::None => { self.to_tail(value) },
+            Self::Tail { value:_ } => {  },
+            Self::Link { value:_, next:_ } => {
+                let old_head = std::mem::replace(self, Node::None);
+                let mut n = Node::Link{ value, next: Box::new(old_head)};
+                std::mem::swap(self, &mut n);
+            },
+        }
+    }
+
+    pub fn get(&self, i: usize) -> Option<&T> {
+        if i == 0 {
+            return match self {
+                Self::Tail { value } => Some(value),
+                Self::Link { value, next:_ } => Some(value),
+                Self::None => None,
+            }
+        } else {
+            return match self {
+                Self::Tail { value:_ } => None,
+                Self::Link { value:_, next } => next.get(i - 1),
+                Self::None => None,
+            }
         }
     }
 
@@ -31,8 +59,33 @@ impl<T> Node<T> where T: Copy {
         }
     }
 
+    pub fn rpop(&mut self) -> Option<T> {
+        match self {
+            Self::None => None,
+            Self::Tail { value } => {
+                let value = *value;
+                self.to_none();
+                Some(value)
+            },
+            Self::Link { value:_, next } => {
+                match **next {
+                    Self::Tail { value: tailvalue } => { self.to_tail_without_value(); Some(tailvalue) },
+                    Self::Link { value:_, next:_ } => next.rpop(),
+                    _ => panic!("Link should never end in None")
+                }
+            }
+        }
+    }
+
     fn to_none(&mut self) {
         *self = Self::None
+    }
+
+    fn to_tail_without_value(&mut self) {
+        *self = match self {
+            Self::Link {value, next:_} => Self::Tail { value: *value },
+            _ => panic!("Cannot convert tail to tail"),
+        }
     }
 
     fn to_tail(&mut self, value: T) {
@@ -65,5 +118,52 @@ mod tests {
 
     #[test]
     fn test_add_single_element() {
+        let mut list = Node::None;
+        list.push(1);
+        assert_eq!(1, list.pop().unwrap());
+    }
+
+    #[test]
+    fn test_add_multiple_elements() {
+        let mut list = Node::None;
+        list.push(1);
+        list.push(2);
+        list.push(3);
+        assert_eq!(1, list.pop().unwrap());
+        assert_eq!(2, list.pop().unwrap());
+        assert_eq!(3, list.pop().unwrap());
+    }
+
+    #[test]
+    fn test_push_left() {
+        let mut list = Node::None;
+        list.push(1);
+        list.push(2);
+        list.lpush(3);
+        assert_eq!(3, list.pop().unwrap());
+        assert_eq!(1, list.pop().unwrap());
+        assert_eq!(2, list.pop().unwrap());
+    }
+
+    #[test]
+    fn test_pop_right() {
+        let mut list = Node::None;
+        list.push(1);
+        list.push(2);
+        list.push(3);
+        assert_eq!(3, list.rpop().unwrap());
+        assert_eq!(2, list.rpop().unwrap());
+        assert_eq!(1, list.rpop().unwrap());
+    }
+
+    #[test]
+    fn test_get() {
+        let mut list = Node::None;
+        list.push(1);
+        list.push(2);
+        list.push(3);
+        assert_eq!(3, *list.get(2).unwrap());
+        assert_eq!(1, *list.get(0).unwrap());
+        assert_eq!(2, *list.get(1).unwrap());
     }
 }
